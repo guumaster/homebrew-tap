48019068cc9636c8a019938623ec9c862fc8e0b1
goreleaserbot <guuweb@gmail.com>
2021-07-13 13:44:31 +0200

    Brew formula update for hostctl version v1.1.0

--- hostctl.rb
+++ hostctl.rb
@@ -11,14 +11,14 @@ class Hostctl < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/guumaster/hostctl/releases/download/v1.1.0/hostctl_1.1.0_macOS_64-bit.tar.gz"
-      sha256 "5281eec5d905215585231730e4aa74d9444f45f0ed4354c47e1e6ad6d2b1ef7d"
+      sha256 "602e8610d3d8de4adb58cc9294683b87e2a795cae635f6135a5291a9fca6c157"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/guumaster/hostctl/releases/download/v1.1.0/hostctl_1.1.0_linux_64-bit.tar.gz"
-      sha256 "231c97b3e17c8d6367baab1c0a9b141b4d3fadaf63629bc1d0665308535383a0"
+      sha256 "68b3ad82317291a8bb1ed36eb43595c32dde4228bd19312d5dcd9f712103bf66"
     end
   end
 
