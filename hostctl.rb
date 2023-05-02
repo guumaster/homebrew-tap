68233f5f0df52ee568074ac31fca690717c22d20
goreleaserbot <guuweb@gmail.com>
2023-05-02 14:26:29 +0200

    Brew formula update for hostctl version v1.1.4

--- hostctl.rb
+++ hostctl.rb
@@ -5,20 +5,20 @@
 class Hostctl < Formula
   desc "Your dev tool to manage /etc/hosts like a pro"
   homepage "https://github.com/guumaster/hostctl"
-  version "1.1.3"
+  version "1.1.4"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/guumaster/hostctl/releases/download/v1.1.3/hostctl_1.1.3_macOS_64-bit.tar.gz"
-      sha256 "34e7a04286fe2b68ef81e4a94270c8e082fb3717be5f9ca7e2469ceade32c142"
+      url "https://github.com/guumaster/hostctl/releases/download/v1.1.4/hostctl_1.1.4_macOS_64-bit.tar.gz"
+      sha256 "f55d36af4223dbf55f7402b3074a4861e5dc63097ccb8f6a91b7f2acc1036cfe"
 
       def install
         bin.install "hostctl"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/guumaster/hostctl/releases/download/v1.1.3/hostctl_1.1.3_macOS_arm64.tar.gz"
-      sha256 "585897bc975a6c060e9a42ab26a331b5e321e56cde7d50271a3a7ad0122d4392"
+      url "https://github.com/guumaster/hostctl/releases/download/v1.1.4/hostctl_1.1.4_macOS_arm64.tar.gz"
+      sha256 "151f19dd77403cce20760ca97bf27d9b5a20b93e0b32f5caf62c5089e1648d4b"
 
       def install
         bin.install "hostctl"
@@ -28,16 +28,16 @@ class Hostctl < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/guumaster/hostctl/releases/download/v1.1.3/hostctl_1.1.3_linux_arm64.tar.gz"
-      sha256 "eeabc7f88d3e35c3d69ee41c4695fc413cf419e40ed6e8bb4d753a63b7b6b768"
+      url "https://github.com/guumaster/hostctl/releases/download/v1.1.4/hostctl_1.1.4_linux_arm64.tar.gz"
+      sha256 "9b3053ac8a0c7de535c07979d1e76b2c5cbeeb44434d2335d3e0c3e776121a9c"
 
       def install
         bin.install "hostctl"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/guumaster/hostctl/releases/download/v1.1.3/hostctl_1.1.3_linux_64-bit.tar.gz"
-      sha256 "1dbcbd415b330e599601b6e7a27be6078bde9cc8ea1c09da4d4781f0b8349718"
+      url "https://github.com/guumaster/hostctl/releases/download/v1.1.4/hostctl_1.1.4_linux_64-bit.tar.gz"
+      sha256 "f4ec3c9f9d54633511251834ce98d409464fb1c0d970f9c865a880e1160db30f"
 
       def install
         bin.install "hostctl"
@@ -45,8 +45,9 @@ class Hostctl < Formula
     end
   end
 
-  def caveats; <<~EOS
-    Remember to add sudo to modify your hosts file.
-  EOS
+  def caveats
+    <<~EOS
+      Remember to add sudo to modify your hosts file.
+    EOS
   end
 end
